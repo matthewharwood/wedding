009c8bd687760543bf61136ae2a0249504c6b057
matthew harwood <matthhar12@gmail.com>
2017-02-27 21:40:27 -0800

    added angular

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+const module = angular.module('app', ['ui.router']);
+
+console.log(module);
+
+module.config(function($stateProvider) {
+  const about = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  };
+  $stateProvider.state(about);
+});
