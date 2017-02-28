21fc416132e20d987643b161c8df82f6da18d058
Daisy Lew <lew.daisy@gmail.com>
2017-02-27 22:44:18 -0800

    fixed enormous images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,25 @@ const module = angular.module('app', ['ui.router']);
 console.log(module);
 
 module.config(function($stateProvider) {
-  const about = {
+  let routes = [];
+
+  routes.push({
     name: 'home',
     url: '/',
     templateUrl: './home/index.html'
-  };
-  $stateProvider.state(about);
+  });
+
+  routes.push({
+    name: 'visit',
+    url: '/visit',
+    templateUrl: './visit/index.html'
+  });
+
+  routes.push({
+    name: 'photo',
+    url: '/photo',
+    templateUrl: './photo/index.html'
+  });
+  
+  routes.forEach(i => $stateProvider.state(i));
 });
